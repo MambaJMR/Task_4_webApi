1f29b403dee50f6217ba2bf3b0c105ab6026db79
krabo <Krabovich05@gmail.com>
2024-02-15 13:51:13 +0300

    Sql index

--- Task_4_webApi/Models/User.cs
+++ Task_4_webApi/Models/User.cs
@@ -1,10 +1,15 @@
-﻿namespace Task_4_webApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task_4_webApi.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [StringLength(20)]
         public string Name { get; set; } = string.Empty;
+        [StringLength(50)]
         public string? Email { get; set; }
+        [StringLength(20)]
         public string Password { get; set; } = string.Empty;
         public bool IsBloked { get; set; } = false;
         public DateTime RegsitrationDate { get; set; }
